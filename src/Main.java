7a3374c7ff82a7d09854d8d03eb1daf553492b0e
Felipe Forte <felipeforte@protonmail.com>
2023-11-29 10:29:39 -0300

    Atualizando com leitura de arquivos graças ao querido Iuri

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,47 @@
-import Estruturas.ArvoreAVL;
+import Estruturas.ListaDinamica;
 import Estruturas.TabelaHash;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
+
     public static void main(String[] args) {
         TabelaHash tabela = new TabelaHash();
-        // RES
-        // RDS
-        // RDE
-        // RDD
-        tabela.adicionar("and",3);
-        tabela.adicionar("amo",3);
-        tabela.adicionar("and",4);
-        tabela.adicionar("and",5);
-        tabela.adicionar("bolha",11);
-        tabela.adicionar("xpto",2);
-        tabela.adicionar("tchonga",6);
-        tabela.adicionar("fonga",12);
-        tabela.adicionar("momo",1);
-        tabela.adicionar("caralho",7);
-        tabela.adicionar("teucu",9);
+        ListaDinamica palavras = new ListaDinamica();
 
+        // Ler palavras-chave
+        String arqPalavras = "/home/felipeforte/Downloads/exemplo_palavras-chave.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(arqPalavras))) {
+            String linha;
 
+            // Lê cada linha do arquivo
+            while ((linha = br.readLine()) != null) {
+                palavras.adiciona(linha.toLowerCase());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String nomeArquivo = "/home/felipeforte/Downloads/exemplo_texto.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(nomeArquivo))) {
+            String linha;
+            int cont = 1;
+
+            // Lê cada linha do arquivo
+            while ((linha = br.readLine()) != null) {
+                // Faça o processamento necessário com cada linha
+                String[] split = linha.split(" ");
+                for (int i=0;i < split.length;i++) {
+                    if (palavras.contem(split[i].toLowerCase())) {
+                        tabela.adicionar(split[i].toLowerCase(),cont);
+                    }
+                }
+                cont++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println(tabela);
     }
 }
